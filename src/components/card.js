71707ac6c5a08807ca3ecd269ef6cff0e8a8dc04
Bixapb <Bixapb@gmail.com>
2021-10-13 23:30:08 +0300

    addOrDeleteList

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,41 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+  Button,
+  CardActions,
+} from "@mui/material/";
 
-export default function ActionAreaCard({ title, overview, id, poster }) {
+export default function ActionAreaCard({
+  title,
+  overview,
+  id,
+  poster,
+  handleButton,
+}) {
+  const [addMovie, setAddMovie] = useState(false);
+
   return (
-    <Card key={id} sx={{ maxWidth: 345, height: "100%" }}>
-      <CardActionArea>
+    <Card
+      key={id}
+      id={id}
+      sx={{
+        maxWidth: 345,
+        height: "100%",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          height: "95%",
+          display: "flex",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+        }}
+      >
         <CardMedia
           component="img"
           height="auto"
@@ -24,6 +51,34 @@ export default function ActionAreaCard({ title, overview, id, poster }) {
           </Typography>
         </CardContent>
       </CardActionArea>
+      <CardActions
+        sx={{
+          height: "5%",
+          width: "100%",
+          m: "0px auto",
+          p: "0px",
+        }}
+      >
+        <Button
+          sx={{
+            height: "100%",
+            width: "100%",
+            flex: 1,
+          }}
+          size="small"
+          color="primary"
+          onClick={(event) => {
+            handleButton(id);
+            addMovie
+              ? (event.target.innerText = "Add to watch list") &&
+                setAddMovie(!addMovie)
+              : (event.target.innerText = "Delete to watch list") &&
+                setAddMovie(!addMovie);
+          }}
+        >
+          Add to watch list
+        </Button>
+      </CardActions>
     </Card>
   );
 }
